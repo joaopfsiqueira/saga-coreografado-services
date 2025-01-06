c105bede2314c69057865bb825f517a59d8bb538
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-06 17:00:42 -0300

    feat: removing orchestrator

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EEventSource.java
+++
@@ -1,9 +0,0 @@
-package br.com.microservices.orchestrated.orchestratorservice.core.enums;
-
-// Enum que representa a origem do evento
-public enum EEventSource {
-    ORCHESTRATOR,
-    PRODUCT_VALIDATION_SERVICE,
-    PAYMENT_SERVICE,
-    INVENTORY_SERVICE
-}
