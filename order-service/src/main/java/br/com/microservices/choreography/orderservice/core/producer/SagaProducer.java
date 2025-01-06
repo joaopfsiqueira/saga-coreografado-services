f85a98ecff0dc981d519d61109b4de07b6281932
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-06 17:27:23 -0300

    feat: producer update

--- order-service/src/main/java/br/com/microservices/choreography/orderservice/core/producer/SagaProducer.java
+++ order-service/src/main/java/br/com/microservices/choreography/orderservice/core/producer/SagaProducer.java
@@ -13,15 +13,15 @@ public class SagaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Value("${spring.kafka.topic.start-saga}")
-    private String startSagaTopic;
+    @Value("${spring.kafka.topic.product-validation-start}")
+    private String productValidationStartTopic;
 
     public void sendEvent(String payload){
         try {
-            log.info("Sending event to topic {} with data {}", startSagaTopic, payload);
-            kafkaTemplate.send(startSagaTopic, payload);
+            log.info("Sending event to topic {} with data {}", productValidationStartTopic, payload);
+            kafkaTemplate.send(productValidationStartTopic, payload);
         } catch (Exception e) {
-            log.error("Error sending event to topic {} with data {}", startSagaTopic, payload, e);
+            log.error("Error sending event to topic {} with data {}", productValidationStartTopic, payload, e);
         }
     }
 }
